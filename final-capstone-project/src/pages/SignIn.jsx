7571494c7a482a7669cfb6e8d878359ce4cb96d3
nsishuba <nalisishuba@gmail.com>
2023-07-28 16:15:25 +0200

    sign in

--- final-capstone-project/src/pages/SignIn.jsx
+++ final-capstone-project/src/pages/SignIn.jsx
@@ -1,29 +1,29 @@
-import { createClient } from "@supabase/supabase-js";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useNavigate } from "react-router-dom";
+// import { createClient } from "@supabase/supabase-js";
+// import { Auth } from "@supabase/auth-ui-react";
+// import { ThemeSupa } from '@supabase/auth-ui-shared'
+// import { useNavigate } from "react-router-dom";
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+// const supabase = createClient(
+//   import.meta.env.VITE_SUPABASE_URL,
+//   import.meta.env.VITE_SUPABASE_ANON_KEY
+// );
 
-export default function SignIn() {
-    const navigate = useNavigate();
+// export default function SignIn() {
+//     const navigate = useNavigate();
 
-    supabase.auth.onAuthStateChange(async (event) => {
-        if (event !== "SIGNED_OUT") {
-            navigate("/success")
-        }else {
-            navigate("/")
-        }
-    })
+//     supabase.auth.onAuthStateChange(async (event) => {
+//         if (event !== "SIGNED_OUT") {
+//             navigate("/success")
+//         }else {
+//             navigate("/")
+//         }
+//     })
 
-      return (
-        <div>
-          <Auth 
-                supabaseClient={supabase}
-                appearance={{ theme: ThemeSupa }}/>
-        </div>
-      );
-}
+//       return (
+//         <div>
+//           <Auth 
+//                 supabaseClient={supabase}
+//                 appearance={{ theme: ThemeSupa }}/>
+//         </div>
+//       );
+// }
